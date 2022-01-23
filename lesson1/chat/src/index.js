5d2a636a26cbfde183cda8d1630d9667a13c9af2
Smirnov <bestalwaysdie@gmail.com>
2022-01-23 15:17:11 +0300

    homework2

--- lesson1/chat/src/index.js
+++ lesson1/chat/src/index.js
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import styles from "./index.module.css";
 
-function Message({ text }) {
+const MessageList = () => {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const lastMessage = messages[messages.length - 1];
+    let timerId = null;
+
+    if (lastMessage?.author !== "Bot" && messages.length) {
+      timerId = setTimeout(() => {
+        setMessages([
+          ...messages,
+          { author: "Bot", message: "Hello from Bot" },
+        ]);
+      }, 500);
+    }
+    return () => clearInterval(timerId);
+  }, [messages]);
+
+  const sendMessage = () => {
+    if (message) {
+      setMessages([...messages, { author: "User", message }]);
+      setMessage("");
+    } else {
+      alert("Введите сообщение");
+    }
+  };
+
   return (
     <div>
-      <h1 className={styles.message__text}>{text}</h1>
+      <h1>MessageList</h1>
+      <input
+        onChange={(event) => setMessage(event.target.value)}
+        placeholder="enter message..."
+        value={message}
+      />
+      <button onClick={sendMessage}>send</button>
+      <hr />
+      {messages.map((message) => (
+        <div>
+          <h2>{message.author}</h2>
+          <p>{message.message}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 function App() {
-  const text = "Переданный пропсом техт-константа";
   return (
     <div>
-      <Message text={text} />
+      <MessageList />
     </div>
   );
 }
