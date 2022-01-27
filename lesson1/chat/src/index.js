f440bbbd9a27980900c64632c00825710ebd7fd8
Smirnov <bestalwaysdie@gmail.com>
2022-01-27 14:53:35 +0300

    homework 3

--- lesson1/chat/src/index.js
+++ lesson1/chat/src/index.js
@@ -1,66 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { MessageList, ChatList, Header, Layout } from "./components";
+import "./palette.css";
 
-const MessageList = () => {
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
+// function App() {
+//   return (
+//     <div>
+//       <MessageList />
+//       <ChatList />
+//     </div>
+//   );
+// }
 
-  useEffect(() => {
-    const lastMessage = messages[messages.length - 1];
-    let timerId = null;
-
-    if (lastMessage?.author !== "Bot" && messages.length) {
-      timerId = setTimeout(() => {
-        setMessages([
-          ...messages,
-          { author: "Bot", message: "Hello from Bot" },
-        ]);
-      }, 500);
-    }
-    return () => clearInterval(timerId);
-  }, [messages]);
-
-  const sendMessage = () => {
-    if (message) {
-      setMessages([...messages, { author: "User", message }]);
-      setMessage("");
-    } else {
-      alert("Введите сообщение");
-    }
-  };
-
-  return (
-    <div>
-      <h1>MessageList</h1>
-      <input
-        onChange={(event) => setMessage(event.target.value)}
-        placeholder="enter message..."
-        value={message}
-      />
-      <button onClick={sendMessage}>send</button>
-      <hr />
-      {messages.map((message) => (
-        <div>
-          <h2>{message.author}</h2>
-          <p>{message.message}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-function App() {
-  return (
-    <div>
-      <MessageList />
-    </div>
-  );
-}
+const theme = createTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Layout
+        header={<Header />}
+        chats={<ChatList />}
+        messages={<MessageList />}
+      />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
