7549cc28a8512582de10b7cad3215dc03790e06e
Smirnov <bestalwaysdie@gmail.com>
2022-02-03 19:28:58 +0300

    homework 5

--- lesson1/chat/src/index.js
+++ lesson1/chat/src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { Header } from "./components";
+import { store } from "./store";
 import { ChatPage, ProfilePage } from "./pages";
 import "./palette.css";
 
@@ -19,16 +21,18 @@ const theme = createTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/chat/*" element={<ChatPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/" element={<h1> Home Page</h1>} />
-        <Route path="/*" element={<h1>Server not found. Error 404</h1>} />
-      </Routes>
-    </BrowserRouter>
-    <ThemeProvider theme={theme}></ThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/chat/*" element={<ChatPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/" element={<h1> Home Page</h1>} />
+          <Route path="/*" element={<h1>Server not found. Error 404</h1>} />
+        </Routes>
+      </BrowserRouter>
+      <ThemeProvider theme={theme}></ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ lesson1/chat/src/store/profile/types.js
@@ -0,0 +1,2 @@
+export const TOGGLE_IS_VISIBLE = "toggle_is_visible";
+export const UPDATE_PROFILE_DATA = "update_profile_data";
